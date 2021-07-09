0d3cb969902cf0e8164ee641372cddfd815e456e
Leonardo Maciel <leonardovidal@gec.inatel.br>
2021-07-09 10:23:03 -0300

    Start Commit

---
+++ app/src/main/java/com/example/cliente_comunication/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.cliente_comunication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = (EditText) findViewById(R.id.textmessage);
+
+        Thread myThread = new Thread(new MyServerThread());
+        myThread.start();
+    }
+
+    class MyServerThread implements Runnable
+    {
+        Socket s;
+        
+        ServerSocket ss;
+        InputStreamReader isr;
+        BufferedReader bufferedReader;
+        Handler h = new Handler();
+
+        String message;
+        Byte yoo;
+
+
+        @Override
+        public void run() {
+            try
+            {
+                ss = new ServerSocket(12345);
+                while (true)
+                {
+                    s = ss.accept();
+                    isr = new InputStreamReader(s.getInputStream());
+                    bufferedReader = new BufferedReader(isr);
+                    message = bufferedReader.readLine();
+                    h.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    });
+
+
+                }
+            }catch(IOException e)
+
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(View v){
+
+        MessageSender messageSender = new MessageSender();
+        messageSender.execute(e1.getText().toString());
+
+    }
+
+}
